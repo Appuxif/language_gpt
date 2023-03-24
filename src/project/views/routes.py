6cd46860e4cb9e1ad91d51c11e54be8f81dd66fd
Smokost <smokost@gmail.com>
2023-03-24 17:55:15 +0100

    Add Main View

--- src/project/views/routes.py
+++ src/project/views/routes.py
@@ -1,4 +1,5 @@
 from project.core.views.base import Route
 from project.views.dummy import DummyView
+from project.views.main import MainView
 
-routes = [Route(DummyView)]
+routes = [Route(DummyView), Route(MainView)]
