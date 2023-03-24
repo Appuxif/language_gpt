9519208a7325d0614a6e3a870161895f31912005
Smokost <smokost@gmail.com>
2023-03-24 17:15:03 +0100

    Add new bot engine base classes

--- src/project/main.py
+++ src/project/main.py
@@ -4,38 +4,25 @@ from logging import getLogger
 from telebot.types import CallbackQuery, Message
 
 from project.core.bot import bot
-from project.db.models.users import UserStateEmpty
+from project.core.views.base import Request
+from project.core.views.dispatcher import ViewDispatcher
 from project.db.mongodb import get_database
-from project.services.callback_data_actions import CallbackDataAction
-from project.services.keyboard import PageName, send_keyboard
-from project.services.user_state_actions import UserStateAction
-from project.services.users import UsersService
 
 logger = getLogger(__name__)
 
 
 @bot.message_handler()
 async def message_handler(msg: Message):
-    user = await UsersService.get_user_for_message(msg)
-    if msg.text == '/start':
-        user.state = UserStateEmpty()
-        await send_keyboard(msg, 'Список групп', user, PageName.MAIN)
-        await bot.delete_message(msg.chat.id, msg.message_id)
-    else:
-        await UserStateAction(user.state.type.value).action(user, msg)
-    await user.update()
+    request = Request(msg=msg)
+    await ViewDispatcher(request=request).dispatch()
+    return
 
 
 @bot.callback_query_handler(func=lambda call: True)
 async def callback_query(callback: CallbackQuery):
-    try:
-        user = await UsersService.get_user_for_message(callback)
-
-        await CallbackDataAction.main_action(user, callback)
-
-        await user.update()
-    finally:
-        await bot.answer_callback_query(callback.id, getattr(callback, '__answer__', None))
+    request = Request(callback=callback)
+    await ViewDispatcher(request=request).dispatch()
+    return
 
 
 async def run():
