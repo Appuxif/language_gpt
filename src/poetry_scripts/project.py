9ece97933849579ac091411a3bb926e36c120dc1
Smokost <smokost@gmail.com>
2023-04-07 19:16:08 +0200

    Add project-shell

--- src/poetry_scripts/project.py
+++ src/poetry_scripts/project.py
@@ -1,3 +1,10 @@
 from project.main import run_loop
 
+try:
+    from IPython import start_ipython as _run_project_shell
+except ImportError:
+    from code import interact as _run_project_shell
+
+
+run_project_shell = _run_project_shell
 run_project_dev = run_loop
